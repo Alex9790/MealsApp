178a3a9dc5f477dcdf9d480fec0e7f93a740df25
Darkestt <martinez.alex.9790@gmail.com>
2020-02-27 21:42:32 -0400

    4. Se crea nuevo Widget CategoryItem(), el cual va a contener el titulo que viene desde Dummy Data y se le aplicara un Background con un efecto gradiente.

---
+++ meals_app/lib/category_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  String title;
+  Color color;
+
+  CategoryItem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //se agrgea un poco de optimizacion agregando "const"
+      padding: const EdgeInsets.all(15),
+      child: Text(title),
+      //estilizando el Background
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color.withOpacity(0.7), color],
+          //direccion en la que se aplica el gradient
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        //suavizar las esqinas / borde redondo
+        borderRadius: BorderRadius.circular(15)
+      ),
+    );
+  }
+}
