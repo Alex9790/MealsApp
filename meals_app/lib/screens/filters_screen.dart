1064995423e3aae34dcc88647b1831fc0997e029
Darkestt <martinez.alex.9790@gmail.com>
2020-03-16 21:46:06 -0400

    28. Se agregan swithes para filtrar los alimentos, aun sin efecto

--- meals_app/lib/screens/filters_screen.dart
+++ meals_app/lib/screens/filters_screen.dart
@@ -2,8 +2,30 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = "/filters";
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
+
+  Widget _buildSwitchListTile(String title, String description,
+      bool currentValue, Function updateValue) {
+    //Widget que muestra un Switch con un texto anexo
+    return SwitchListTile(
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(description),
+      //funcion para cambiar el valor del switch
+      onChanged: updateValue,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +34,65 @@ class FiltersScreen extends StatelessWidget {
         title: Text("Filtros"),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text("Filters"),
+      body: Column(
+        children: <Widget>[
+          //servira como titulo
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Ajustar la seleccion de Comidas.",
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          //Widget que se asegura que el Widget hijo cubra tanto espacio como pueda, dentro de la Column/Row que lo contiene
+          Expanded(
+            child: ListView(
+              //se usara el argumento children porque se sabe la cantidad de elementos que contendra la lista
+              children: <Widget>[
+                _buildSwitchListTile(
+                  "Gluten-Free",
+                  "Incluir solo Comidas Gluten-Free",
+                  _glutenFree,
+                  (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile(
+                  "Vegetarian",
+                  "Incluir solo Comidas Vegetarianas",
+                  _vegetarian,
+                  (newValue) {
+                    setState(() {
+                      _vegetarian = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile(
+                  "Vegan",
+                  "Incluir solo Comidas Veganas",
+                  _vegan,
+                  (newValue) {
+                    setState(() {
+                      _vegan = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile(
+                  "Lactose-Free",
+                  "Incluir solo Comidas sin Lactosa",
+                  _lactoseFree,
+                  (newValue) {
+                    setState(() {
+                      _lactoseFree = newValue;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
